3e410dcf6e97bbc3ecd7d8e55d5da8c83831607c
lils <lilsolkaryan@gmail.com>
2024-07-18 21:33:25 +0400

    lesson-14. Add showList function

--- app/src/main/java/com/example/shopinglistxml/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shopinglistxml/presentation/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
 import com.example.shopinglistxml.R
 import com.example.shopinglistxml.domain.ShopItem
@@ -17,14 +14,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var llShopList: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         llShopList = findViewById(R.id.ll_shop_list)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         viewModel.shopList.observe(this){
             showList(it)
