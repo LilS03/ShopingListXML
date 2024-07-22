17dcc37c45b01932ce037f7c90e7c27d68f0324d
lils <lilsolkaryan@gmail.com>
2024-07-22 14:22:16 +0400

    lesson-23. add shopitemactivity

---
+++ app/src/main/java/com/example/shopinglistxml/presentation/ShopItemActivity.kt
@@ -0,0 +1,11 @@
+package com.example.shopinglistxml.presentation
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.shopinglistxml.R
+
+class ShopItemActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
